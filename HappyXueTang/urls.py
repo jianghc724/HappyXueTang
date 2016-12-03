b1e2a02b5807aa304567f24b656ce38558207bb3
DESKTOP-H26ORQ8\JiangHaochen <jianghc724@foxmail.com>
2016-12-03 14:47:40 +0800

    foo

--- HappyXueTang/urls.py
+++ HappyXueTang/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 # from django.contrib import admin
 
 from wechat.views import CustomWeChatView
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url(r'^wechat/?S', CustomWeChatView.as_view()),
+    url(r'^wechat/?$', CustomWeChatView.as_view()),
 ]
