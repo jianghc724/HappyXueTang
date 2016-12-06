382b4aedf245f890941df614bf66d25c0f829a28
DESKTOP-H26ORQ8\JiangHaochen <jianghc724@foxmail.com>
2016-12-06 10:07:08 +0800

    course list frame finished

--- user/views.py
+++ user/views.py
@@ -54,14 +54,37 @@ class CourseList(APIView):
         return_json = r.json()
         if return_json['message'] == 'Success':
             course_json = return_json['classes']
-            course_id = course_json['coursid']
-            courses = Course.objects.filter(course_id = course_id)
+            courseid = course_json['coursid']
+            courses = Course.objects.filter(course_id=courseid)
+            time = self.get_time(course_json['time'])
+
             if not courses:
-                # create course
+                weeks = 0
+                for week in course_json['week']:
+                    weeks = weeks * 2 + week
+                cou = Course.objects.create(name=course_json['coursename'], key=course_json['coursid'], teacher=course_json['teacher'],
+                                            week=weeks, location=course_json['classroom'], course_time=time)
+                cou.save()
+            elif not self.is_time_in_coursetime(courses, time):
+                cou = Course.objects.get(course_id = courseid)
+                pre_time = cou.course_time
+                new_time = pre_time + ',' + time
+                cou.course_time = new_time
+                cou.save()
+
+            stucou = StudentCourse.objects.filter(student_id=userid).filter(course_key=courseid)
+            if not stucou:
+                stu_cou = StudentCourse.objects.create(student_id=userid, course_key=courseid)
+                stu_cou.save()
                 pass
-            # create student course
         else:
             # raise proper error
+            pass
+
+        def get_time(self, times):
+            pass
+
+        def is_time_in_coursetime(self, cou, time):
             pass
 
 
