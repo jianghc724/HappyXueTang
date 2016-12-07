fbdd078a89761cb01d1dfe144f0b6232b6b2a7b5
DESKTOP-H26ORQ8\JiangHaochen <jianghc724@foxmail.com>
2016-12-08 03:12:21 +0800

    unbind fix

--- wechat/handlers.py
+++ wechat/handlers.py
@@ -58,7 +58,7 @@ class UnbindOrUnsubscribeHandler(WeChatHandler):
         }
         headers = {'content-type': 'application/json'}
         userid = User.get_by_openid(self.user.open_id).user_id
-        addr = 'http://se.zhuangty.com:8000/curriculum/' + userid + '?username=' + userid
+        addr = 'http://se.zhuangty.com:8000/users/' + userid + '?username=' + userid
         r = requests.post(addr, data=data, headers=headers)
         return_json = r.json()
         if return_json['message'] == 'Success':
