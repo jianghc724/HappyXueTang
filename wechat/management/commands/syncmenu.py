55f44ed95286e42bd960230b1b73aa9c989b393b
CaoZhilin <caozhilin14@gmail.com>
2016-12-07 16:41:52 +0800

    menu update02

---
+++ wechat/management/commands/syncmenu.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+import logging
+
+from django.utils import timezone
+from django.core.management.base import BaseCommand, CommandError
+
+from wechat.views import CustomWeChatView
+
+
+__author__ = "Epsirom"
+
+
+class Command(BaseCommand):
+    help = 'Automatically synchronize WeChat menu'
+
+    logger = logging.getLogger('syncmenu')
+
+    def handle(self, *args, **options):
+        CustomWeChatView.update_menu()
+
+Command.logger.setLevel(logging.DEBUG)
