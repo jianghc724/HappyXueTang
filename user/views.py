303fd96d34779a0512309c6d2b255656155014fd
sal129 <sal_129@163.com>
2016-12-15 15:38:02 +0800

    views.py usernotify

--- user/views.py
+++ user/views.py
@@ -179,3 +179,81 @@ class GetDeadline(APIView):
                         raise LogicError('Unknown error')
             return result
         raise LogicError('Username Invalid')
+
+
+class UserNotify(APIView):
+    def get(self):
+        self.check_input('open_id')
+        data = {
+            "apikey": API_KEY,
+            "apisecret": API_SECRET,
+        }
+        headers = {'content-type': 'application/json'}
+        userid = User.get_by_openid(self.input['open_id']).user_id
+        addr = 'http://se.zhuangty.com:8000/learnhelper/' + userid + '/courses?username=' + userid
+        r = requests.post(addr, data=json.dumps(data), headers=headers)
+        return_json = r.json()
+        current_time = datetime.now().timestamp()
+        result = {
+            "total": {}, #字典，显示用户全部的未读公告数、未下载文件数和未交作业数
+            "new_detail": {
+                "new_notices": [],
+                "new_files": [],
+                "new_operations": [],
+            }, #字典，存储所有未读公告、未下载文件和未交作业
+        }
+        if return_json['message'] == "success":
+            courses = return_json['courses']
+            total_unreadnotice = 0
+            total_newfile = 0
+            total_unsubmittedoperations = 0
+            for course in courses:
+                total_unreadnotice += course['unreadnotice']
+                total_newfile += course['newfile']
+                total_unsubmittedoperations += course['unsubmittedoperations']
+                courseid = course['courseid']
+                notice_addr = 'http://se.zhuangty.com:8000/learnhelper/' + userid + '/courses/' + courseid + '/notices?username=' + userid
+                file_addr = 'http://se.zhuangty.com:8000/learnhelper/' + userid + '/courses/' + courseid + '/documents?username=' + userid
+                operation_addr = 'http://se.zhuangty.com:8000/learnhelper/' + userid + '/courses/' + courseid + '/assignments?username=' + userid
+                notice_json = requests.post(notice_addr, data=json.dumps(data), headers=headers).json()
+                file_json = requests.post(file_addr, data=json.dumps(data), headers=headers).json()
+                operation_json = requests.post(operation_addr, data=json.dumps(data), headers=headers).json()
+                if notice_json['message'] == "Failure":
+                    raise LogicError(notice_json['reason'] + "for notice")
+                if file_json['message'] == "Failure":
+                    raise LogicError(file_json['reason'] + "for file")
+                if operation_json['message'] == "Failure":
+                    raise LogicError[operation_json['reason'] + "for operation"]
+                notices = notice_json['notices']
+                for notice in notices:
+                    if notice.state == "unread":
+                        result['new_notices'].append({
+                            'title': notice['title'],
+                            'publishtime': notice['publishtime'],
+                            'content': notice['content'],
+                        })
+                files = file_json['documents']
+                for file in files:
+                    if file.state == "new":
+                        result['new_files'].append({
+                            'title': file['title'],
+                            'explanation': file['explanation'],
+                            'updatingtime': file['updatingtime'],
+                            'size': file['size'],
+                            'url': file['url'],
+                        })
+                operations = operation_json['assignments']
+                for operation in operations:
+                    if operation.state == "尚未提交":
+                        result['new_operations'].append({
+                                'title': operation['title'],
+                                'startdate': operation['startdate'],
+                                'duedate': operation['duedate'],
+                                'detail': operation['detail'],
+                                'fileurl': operation['fileurl'],
+                            })
+            result['total']['total_unreadnotice'] = total_unreadnotice
+            result['total']['total_newfile'] = total_newfile
+            result['total']['total_unsubmittedoperations'] = total_unsubmittedoperations
+        else:
+            raise LogicError('Username Invalid')
