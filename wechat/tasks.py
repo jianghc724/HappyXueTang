b8f2d865b5a57cbca4cad9e712072d3c120b4b2d
DESKTOP-H26ORQ8\JiangHaochen <jianghc724@foxmail.com>
2016-12-29 06:33:21 +0800

    celery

--- wechat/tasks.py
+++ wechat/tasks.py
@@ -39,7 +39,7 @@ def get_notice():
         access_json = r.json()
         print(access_json)
         access_token = access_json['access_token']
-        str = "您还有" + str(total_notice) + "个未读公告，" + str(total_homework) + "个未交作业"
+        return_str = "您还有" + str(total_notice) + "个未读公告，" + str(total_homework) + "个未交作业"
         print(str)
         we_addr = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=" + access_token
         we_data = {
@@ -47,7 +47,7 @@ def get_notice():
             "msgtype": "text",
             "text":
                 {
-                    "content": str
+                    "content": return_str
                 }
 
         }
