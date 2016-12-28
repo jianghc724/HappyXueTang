ce10a9b6a84514a70c3a313090b9042f9f8bd49a
DESKTOP-H26ORQ8\JiangHaochen <jianghc724@foxmail.com>
2016-12-29 04:53:45 +0800

    Revert "celery"
    This reverts commit 695d40fd0c727aed5eafd0831d83473e831fe24b.

--- wechat/tasks.py
+++ wechat/tasks.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import
-import logging
 from HappyXueTang.celery import app
-from celery.utils.log import get_task_logger
 from wechat.handlers import *
 from wechat.wrapper import *
 from wechat.models import *
@@ -11,8 +9,6 @@ from HappyXueTang.settings import WECHAT_APPID
 
 @app.task(name='wechat.tasks.get_notice')
 def get_notice():
-    logger = get_task_logger(__name__)
-    logger.info('func start  ----------------->')
     users = User.objects.all()
     for user in users:
         if user.user_status != 0:
@@ -27,5 +23,5 @@ def get_notice():
         for handler in WeChatView.handlers:
             inst = handler(WeChatView, msg, user)
             if inst.check():
-                inst.handle()
-        logger.info('func end  ----------------->')
+                return inst.handle()
+        return WeChatView.default_handler(WeChatView, msg, user).handle()
