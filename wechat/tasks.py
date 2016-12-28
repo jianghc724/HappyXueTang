695d40fd0c727aed5eafd0831d83473e831fe24b
DESKTOP-H26ORQ8\JiangHaochen <jianghc724@foxmail.com>
2016-12-29 04:49:43 +0800

    celery

--- wechat/tasks.py
+++ wechat/tasks.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
+import logging
 from HappyXueTang.celery import app
+from celery.utils.log import get_task_logger
 from wechat.handlers import *
 from wechat.wrapper import *
 from wechat.models import *
@@ -9,6 +11,8 @@ from HappyXueTang.settings import WECHAT_APPID
 
 @app.task(name='wechat.tasks.get_notice')
 def get_notice():
+    logger = get_task_logger(__name__)
+    logger.info('func start  ----------------->')
     users = User.objects.all()
     for user in users:
         if user.user_status != 0:
@@ -23,5 +27,5 @@ def get_notice():
         for handler in WeChatView.handlers:
             inst = handler(WeChatView, msg, user)
             if inst.check():
-                return inst.handle()
-        return WeChatView.default_handler(WeChatView, msg, user).handle()
+                inst.handle()
+        logger.info('func end  ----------------->')
