121108f5a6a476e7693ace04e6df3f29df42b5b7
JiangHaochen <jianghc724@foxmail.com>
2016-12-28 20:29:51 +0800

    get new trend complete

--- wechat/handlers.py
+++ wechat/handlers.py
@@ -186,9 +186,10 @@ class GetNewNoticeHandler(WeChatHandler):
             total_homework = 0
             course_json = return_json['courses']
             for course in course_json:
-                total_homework = total_homework + course['unsubmittedopertions']
+                print (course)
+                total_homework = total_homework + course['unsubmittedoperations']
                 total_notice = total_notice + course['unreadnotice']
-            self.reply_text("您还有" + str(total_notice) + "个未读公告，" + str(total_homework) + "个未交作业")
+            return self.reply_text("您还有" + str(total_notice) + "个未读公告，" + str(total_homework) + "个未交作业")
 
 
 '''class NotifyAllUserHandler(WeChatHandler):
@@ -215,6 +216,6 @@ class GetNewNoticeHandler(WeChatHandler):
                 total_homework = 0
                 course_json = return_json['courses']
                 for course in course_json:
-                    total_homework = total_homework + course['unsubmittedopertions']
+                    total_homework = total_homework + course['unsubmittedoperations']
                     total_notice = total_notice + course['unreadnotice']
                 self.reply_text("您还有"+ str(total_notice) + "个未读公告，" + str(total_homework) + "个未交作业")'''
