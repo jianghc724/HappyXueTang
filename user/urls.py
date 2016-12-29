635266b7c25212ca645dd93187e29ecd2be93481
CaoZhilin <caozhilin14@gmail.com>
2016-12-29 10:58:47 +0800

    merge url

--- user/urls.py
+++ user/urls.py
@@ -7,12 +7,11 @@ import user.views
 urlpatterns = [
     url(r'^user/bind?$', user.views.UserBind.as_view()),
     url(r'^course/list?$', user.views.CourseList.as_view()),
-<<<<<<< HEAD
+
     url(r'^course/detail?$', user.views.CourseDetail.as_view()),
     url(r'^course/comments/overview?$', user.views.CommentOverview.as_view()),
     url(r'^course/notices?$', user.views.GetNotice.as_view()),
-=======
->>>>>>> parent of 3054fd4... Merge branch 'master' of github.com:jianghc724/HappyXueTang
+
     url(r'^ddl/list?$', user.views.GetDeadline.as_view()),
     url(r'^library/status?$', user.views.LibraryStatus.as_view()),
     url(r'^bullet/screen?$', user.views.BulletScreen.as_view()),
