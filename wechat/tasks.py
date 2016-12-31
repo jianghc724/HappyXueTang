05e8a764607e22bea91ecccd2662b4d91ab5c20c
DESKTOP-H26ORQ8\JiangHaochen <jianghc724@foxmail.com>
2016-12-31 13:19:35 +0800

    celery

--- wechat/tasks.py
+++ wechat/tasks.py
@@ -12,7 +12,7 @@ from HappyXueTang.settings import WECHAT_APPID, WECHAT_SECRET
 def get_notice():
     users = User.objects.all()
     for user in users:
-        # print(user.user_id)
+        print(user.user_id)
         if user.user_status != 0:
             continue
         userid = user.user_id
@@ -29,7 +29,7 @@ def get_notice():
             total_homework = 0
             course_json = return_json['courses']
             for course in course_json:
-                # print(course)
+                print(course)
                 total_homework = total_homework + course['unsubmittedoperations']
                 total_notice = total_notice + course['unreadnotice']
             ac_addr = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="\
